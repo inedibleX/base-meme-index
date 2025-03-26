7b51e0932744bb94da4dd88e8414ccc0b7802c6c
RobertMCForster <rforster510@yahoo.com>
2025-03-26 01:55:06 -0700

    Updated Pool ID

--- src/app/(main)/hooks/use-bmi-token-usd-price-and-tvl.ts
+++ src/app/(main)/hooks/use-bmi-token-usd-price-and-tvl.ts
@@ -13,7 +13,7 @@ export const useBmiTokenUsdPriceAndTVL = () => {
   const { data: tokenInfo, isLoading: isTokenInfoLoading } =
     useReadVaultGetPoolTokenInfo({
       args: [
-        '0xb8931645216d8ff2b4d8323a6bbbef9bd482db350001000000000000000001d2',
+        '0xb8931645216d8ff2b4d8323a6bbbef9bd482db350001000000000000000001d7',
         '0x0d97f261b1e88845184f678e2d1e7a98d9fd38de',
       ],
     })
