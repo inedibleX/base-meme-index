705a3e25f5b2b9c18e874c38591825d4f2b947b4
RobertMCForster <rforster510@yahoo.com>
2025-03-25 21:47:13 -0700

    Update Header

--- src/app/(main)/components/header.tsx
+++ src/app/(main)/components/header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
         <p className="mx-auto max-w-3xl text-lg leading-relaxed text-white/90">
           Base&apos;s first equal-weighted meme index fund! Deposit Ether and
           receive equal amounts of 8 top Base memes that constantly rebalance.
-          Equal weighting has more volatility than cap weighting--as memes
+          Equal weighting has more upside potential than cap weighting--as memes
           should!
         </p>
       </div>
